bdcf693eeca602568fcdc895ec6595cc8e480f59
Chris Zavala <frappejcat@gmail.com>
2023-01-30 19:02:18 -0600

    adding notesRoutes.js file.

---
+++ routes/api/notesRoutes.js
@@ -0,0 +1,22 @@
+//Variables Declarations: 
+const router = require('express').Router();
+let { noteArray } = require('../../db/db.json');
+const { createNewnote } = require('../../lib/notes');
+
+
+//the note is at api/notes being a JSON
+router.get('/notes', (req, res) => {
+    let data = noteArray;
+    res.json(data);
+});
+
+router.post('/notes', (req, res) => {
+    if(noteArray){
+        //for the next index of the array. 
+        req.body.id = noteArray.length.toString();
+    }else 
+        {req.body.id = 0}
+        res.json(createNewnote(req.body, noteArray));
+});
+//this exports the module. 
+module.exports = router; 
