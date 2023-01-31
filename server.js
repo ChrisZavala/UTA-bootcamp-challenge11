db214c57f6b3bb29b344c7fe8a1f1f5408a38587
Chris Zavala <frappejcat@gmail.com>
2023-01-30 18:26:34 -0600

    added the server.js and finshed it I think.

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express = require('express');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
-
+//need variables for our routes
+const htmlRoutes = require ("./routes/html");
+const apiRoutes = require("./routes/api");
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
-
-//routes we are going to use?
-
-
-
-
+//routes we are going to use based on variable above. 
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 //love the rocket!
 app.listen(PORT, () =>
